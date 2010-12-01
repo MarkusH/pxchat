7ce4fdfcc2001bd732d266bfd0e73bd7fe0624d6
fbausch <studium@fbausch.de>
2010-12-01 16:39:50 +0100

    Test image in splash screen, menu bar in main window.

--- src/pxchat/gui/ClientMain.java
+++ src/pxchat/gui/ClientMain.java
@@ -1,19 +1,40 @@
 package pxchat.gui;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class ClientMain extends JFrame{
 	public static ClientMain mainWindow = null;
 	public ClientMain() {
 		super("pxchat");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		JMenuBar mBar = new JMenuBar();
+		JMenu mFile = new JMenu("Datei");
+		JMenuItem mExit = new JMenuItem("Beenden");
+		mExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ClientMain.this.dispose();
+			}
+		});
+		mFile.add(mExit);
+		mBar.add(mFile);
+		this.setJMenuBar(mBar);
 		this.setSize(500, 320);
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(size.width/2-this.getWidth()/2, size.height/2-this.getHeight()/2);
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		mainWindow = new ClientMain();
 		new SplashScreen().setVisible(true);
 		
--- src/pxchat/gui/SplashScreen.java
+++ src/pxchat/gui/SplashScreen.java
@@ -1,26 +1,41 @@
 package pxchat.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+import javax.swing.SwingConstants;
 
 public class SplashScreen extends JFrame {
 	public SplashScreen () {
-		super("enter pxchat");
-		JButton startButton = new JButton("enter chat");
+		super("pxchat");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		JButton startButton = new JButton("pxchat betreten");
 		startButton.setPreferredSize(new Dimension(200,30));
 		startButton.addActionListener(new ActionListener() {
-			
-			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				SplashScreen.this.setVisible(false);
+				SplashScreen.this.dispose();
 				ClientMain.mainWindow.setVisible(true);
 			}
 		});
-		this.getContentPane().add(startButton);
+		JLabel imageLabel = new JLabel("",new ImageIcon("testbild.png"), SwingConstants.LEFT);
+		imageLabel.setToolTipText("Prüfen Sie mittels dieses Bildes, ob Ihre Bildschirmeinstellungen stimmen.");
+		this.getContentPane().add(imageLabel, BorderLayout.CENTER);
+		this.getContentPane().add(startButton, BorderLayout.SOUTH);
 		this.pack();
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(size.width/2-this.getWidth()/2, size.height/2-this.getHeight()/2);
+		this.setResizable(false);
 	}
 }
