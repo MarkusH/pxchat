868a9cb9908a4574b9251f84fe3b8e29e1c33629
rwaury <robert.waury@googlemail.com>
2010-12-01 18:01:10 +0100

    IDs added in Frame.java

--- src/pxchat/net/protocol/frames/Frame.java
+++ src/pxchat/net/protocol/frames/Frame.java
@@ -9,9 +9,15 @@ public abstract class Frame implements Serializable {
 	public static final int ID_VERSION = 1;
 	public static final int ID_SID = 2;
 	public static final int ID_MSG = 3;
+	public static final int ID_AUTO = 4;
 	
+	public static final int ID_IMG = 10;
 	
 	public static final int ID_POINT = 100;
+	public static final int ID_LINE = 101;
+	public static final int ID_RECT = 102;
+	public static final int ID_CIRCLE = 103;
+	public static final int ID_IMGTEXT = 104;
 	
 	
 	protected int id = ID_NOP;
