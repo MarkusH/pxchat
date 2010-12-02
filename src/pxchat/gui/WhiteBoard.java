9cd9428f824ce08b4b9f797f533a5bbdb3e2e2c9
fbausch <studium@fbausch.de>
2010-12-02 09:48:06 +0100

    Added WhiteBoard class to gui-packet.

---
+++ src/pxchat/gui/WhiteBoard.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package pxchat.gui;
+
+import javax.swing.JFrame;
+
+/**
+ * @author florian
+ *
+ */
+public class WhiteBoard extends JFrame {
+
+	public WhiteBoard() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
