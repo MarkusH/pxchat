69eadc9aff55b214eea152dd618c2ca767f1a78e
fbausch <studium@fbausch.de>
2010-12-07 21:02:29 +0100

    Added KeyListener to ConnectionDialog so that you can connect by pressing return and cancel by pressing escape; added missing i18n for lineWidthSlider

--- src/pxchat/gui/ConnectionDialog.java
+++ src/pxchat/gui/ConnectionDialog.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -32,6 +34,29 @@ public class ConnectionDialog extends JDialog {
 	private JPasswordField passWord;
 	private JLabel hostAddressLabel, portNumberLabel, userNameLabel, passWordLabel;
 
+	private KeyListener returnKeyListener = new KeyListener() {
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+				ConnectionDialog.this.connect();
+			} else
+				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+					ConnectionDialog.this.abort();
+				}
+		}
+	};
+
 	public ConnectionDialog(ClientMain parent) {
 		super(parent, I18n.getInstance().getString("cdTitle"));
 		this.setModalityType(ModalityType.APPLICATION_MODAL);
@@ -48,6 +73,7 @@ public class ConnectionDialog extends JDialog {
 			}
 		});
 		connectButton.setBounds(35, 150, 100, 30);
+		connectButton.addKeyListener(returnKeyListener);
 
 		abortButton = new JButton(I18n.getInstance().getString("cdAbort"));
 		abortButton.addActionListener(new ActionListener() {
@@ -74,7 +100,8 @@ public class ConnectionDialog extends JDialog {
 			}
 		});
 		hostAddress.setBounds(120, 10, 150, 25);
-		
+		hostAddress.addKeyListener(returnKeyListener);
+
 		hostAddressLabel = new JLabel(I18n.getInstance().getString("cdHost"));
 		hostAddressLabel.setBounds(10, 10, 100, 25);
 
@@ -93,7 +120,8 @@ public class ConnectionDialog extends JDialog {
 			}
 		});
 		portNumber.setBounds(120, 45, 150, 25);
-		
+		portNumber.addKeyListener(returnKeyListener);
+
 		portNumberLabel = new JLabel(I18n.getInstance().getString("cdPort"));
 		portNumberLabel.setBounds(10, 45, 100, 25);
 
@@ -112,7 +140,8 @@ public class ConnectionDialog extends JDialog {
 			}
 		});
 		userName.setBounds(120, 80, 150, 25);
-		
+		userName.addKeyListener(returnKeyListener);
+
 		userNameLabel = new JLabel(I18n.getInstance().getString("cdUser"));
 		userNameLabel.setBounds(10, 80, 100, 25);
 
@@ -131,7 +160,8 @@ public class ConnectionDialog extends JDialog {
 			}
 		});
 		passWord.setBounds(120, 115, 150, 25);
-		
+		passWord.addKeyListener(returnKeyListener);
+
 		passWordLabel = new JLabel(I18n.getInstance().getString("cdPassWord"));
 		passWordLabel.setBounds(10, 115, 100, 25);
 
